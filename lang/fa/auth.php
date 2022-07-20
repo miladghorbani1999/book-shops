977191188684bbfdc0d13c33af4e80d0dca18a5e
MiladGhorbani <miladghorbani006@gmail.com>
2022-07-20 15:31:32 +0430

    add lang persian and deleted files font in resource

--- lang/fa/auth.php
+++ lang/fa/auth.php
@@ -2,10 +2,13 @@
 return[
 
     "main"=>[
-        "max"=>"تعداد کاراکتر های :text از حد مجاز بیشتر است.",
-        "required"=>"وارد کردن :text اجباری است.",
+        "max"=>"تعداد کاراکتر های :attribute از حد مجاز بیشتر است.",
+        "min"=>"تعداد کاراکتر های :attribute کمتر از حد مجاز است.",
+        "required"=>"وارد کردن :attribute اجباری است.",
         "unique"  =>"با این :text قبلا ثبت نام انجام شده است.",
-        "correct" =>":text خود درست وارد کنید",
-        "digit"   => ":attribute وارد شده اشتباه است"
+        "correct" =>":attribute خود درست وارد کنید",
+        "digit"   => ":attribute وارد شده اشتباه است",
+        "integer" => ":attribute باید عدد باشد.",
+        "string" => ":attribute باید متن باشد."
     ]
 ];
