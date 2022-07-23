01aa897e56bfaf35ccc742836a7961b14236f913
MiladGhorbani <miladghorbani006@gmail.com>
2022-07-23 11:13:31 +0430

    edit datetime helper functions

--- app/Helpers/datetime.php
+++ app/Helpers/datetime.php
@@ -3,17 +3,29 @@
 
 use Morilog\Jalali\Jalalian;
 
-function jalali_date($datetime): ?string{
-    if (!$datetime) return null;
-    return jdate($datetime)->format('j F Y');
-}
+if (!function_exists('jalali')) {
+    function jalali(string $datetime):?Jalalian
+    {
+        return jdate($datetime);
+    }
 
-function jalali_date_format2($datetime): ?string{
-    if (!$datetime) return null;
-    return jdate($datetime)->format('Y/m/d');
-}
+    if (function_exists('jalali_date_format2')) {
+        function jalali_date(string $datetime): string
+        {
+            return jalali($datetime)->format('j F Y');
+        }
+    }
 
-function jalali_to_solar(string $datetime): ?string{
+    if (function_exists('jalali_date_format2')) {
+        function jalali_date_format2(string $datetime): string
+        {
+            return jdate($datetime)->format('Y/m/d');
+        }
+    }
+
+}
+function jalali_to_solar(string $datetime): ?string
+{
     return Jalalian::fromFormat('Y/m/d', $datetime)->toCarbon()->format('Y-m-d');
-
 }
+
